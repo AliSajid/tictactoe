1d56699a5dcb3cfbec8c0bf183573ff0f563e02a
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-01-20 15:55:50 -0500

    feat(player): add an independent player trait

--- src/lib_player/src/lib.rs
+++ src/lib_player/src/lib.rs
@@ -41,3 +41,7 @@
 // * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // * SOFTWARE.
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+mod player;
+
+pub use player::Player;
