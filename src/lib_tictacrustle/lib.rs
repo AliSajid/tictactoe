0fb2cb8a3d4bf22d90666688f97cf58c11c3da31
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-08-12 09:10:25 -0400

    build: update the project structure
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- src/lib_tictacrustle/lib.rs
+++ src/lib_tictacrustle/lib.rs
@@ -4,9 +4,11 @@
 // SPDX-License-Identifier: MIT
 
 mod board;
+mod game;
 mod square;
 mod square_value;
 
 pub use board::Board;
+pub use game::Game;
 pub use square::Square;
 pub use square_value::SquareValue;
